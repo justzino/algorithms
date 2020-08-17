74c151b868a182c52c1d6046688746b46133d28c
JMJ <mseagle2023@gmail.com>
2020-08-17 20:36:39 +0900

    ummjevel 복구

--- justzino/in&out/11021.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-for i in range(n):
-    a, b = map(int, input().split())
-    print('Case #{}: {}'.format(i + 1, a + b))
-
-
-# "출력형식"%(데이터....) : 이전 방식
-# "출력형식".format(데이터...) : 파이썬(Python) 3 포맷팅 방식
--- justzino/in&out/8393.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-result = 0
-
-for i in range(1, n + 1):
-    result = result + i
-
-print(result)
