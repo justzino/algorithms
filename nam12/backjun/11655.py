2921e89649010692c1c500485c5a7b802be51b93
simyeen <simyeen@naver.com>
2020-08-23 17:32:57 +0900

    11656.py

--- nam12/backjun/11655.py
+++ nam12/backjun/11655.py
@@ -7,16 +7,21 @@ for i in range(len(string)):
 
     if string[i].isupper():
         if ord(string[i]) + 13 > ord('Z'):
-            ans[i] = ord(string[i])-13
+            ans.append(ord(string[i])-13)
         else:
-            ans[i] = ord(string[i])+13
+            ans.append(ord(string[i])+13)
 
     elif string[i].islower():
         if ord(string[i]) + 13 > ord('z'):
-            ans[i] = ord(string[i])-13
+            ans.append(ord(string[i])-13)
         else:
-            ans[i] = ord(string[i])+13
+            ans.append(ord(string[i])+13)
     else:
-        ans[i] = ord(string[i])
+        ans.append(ord(string[i]))
 
-    print(ans[i], end="")
+for i in range(len(ans)):
+    print(chr(ans[i]), end="")
+
+# 그냥 ans[i] = ord(string[i])+13을 해주면 안되고
+# ans가 list이므로 append로 해줬어야 했다.
+# 빈 list ans에 ans[i]자체가 존재하지 않아서 indexError가 뜬 것 같다.
