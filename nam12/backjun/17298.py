299cdfe04a16ee3899f8052073fe1f74643b76e8
simyeen <simyeen@naver.com>
2020-08-25 02:15:43 +0900

    17298.py

--- nam12/backjun/17298.py
+++ nam12/backjun/17298.py
@@ -5,24 +5,14 @@ N = int(sys.stdin.readline().rstrip())
 arr = list(map(int, sys.stdin.readline().rstrip().split()))
 
 ans = [-1 for i in range(N)]
-max = arr[len(arr)-1]
+stack = []
 
-for i in range(len(arr)-1, 0, -1):
+for i in range(len(arr)):
 
-    if max < arr[i]:
-        max = arr[i]
+    while stack and arr[stack[-1]] < arr[i]:
+        ans[stack.pop()] = arr[i]
 
-    if arr[i-1] < arr[i]:
-        ans[i-1] = arr[i]
+    stack.append(i)
 
-    elif arr[i-1] < ans[i]:
-        ans[i-1] = ans[i]
-
-    elif arr[i-1] < max:
-        ans[i-1] = max
-
-for i in range(N):
-    print(ans[i], end=" ")
-
-# i부터 len(arr)까지 쭉 돌리면서하면 n^2나와서 이렇게함.
-# 아오 왜 안돼 ㅐㅐㅐㅐ
+for i in ans:
+    print(i, end=" ")
