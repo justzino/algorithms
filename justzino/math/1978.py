a024a33c9a0424c9ec70b5b4d904424e482df0cb
Jinho <originalll94@gmail.com>
2020-08-27 21:22:54 +0900

    math NO.1978 소수 구하기

--- justzino/math/1978.py
+++ justzino/math/1978.py
@@ -1,13 +1,28 @@
 import sys
 
 
+def is_prime(num):
+    root_num = num ** 0.5
+    chk = False
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    div = 2
+    while div <= root_num:
+        if num % div == 0:
+            return False
+        div += 1
+    return True
+
+
 n = int(sys.stdin.readline())
 x = list(map(int, sys.stdin.readline().split()))
 
-prime = [2, ]
-check = 3
 n_prime = 0
 
 for i in range(n):
-    for j in range(x[i]):
-        
+    if is_prime(x[i]):
+        n_prime += 1
+
+print(n_prime)
