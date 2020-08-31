ecd07fc5ad604c5fbd91683274f4a8403f402191
Jinho <originalll94@gmail.com>
2020-08-31 18:30:25 +0900

    math NO.2745 진법 변환(n진법 -> 10진법)

---
+++ justzino/math/2745.py
@@ -0,0 +1,18 @@
+# n진법 -> 10진법
+def notation(num, n):
+    n_set = {chr(ord('A')+i-10): i for i in range(10, n)}      # {'A': 10, 'B': 11, 'C': 12, 'D': 13, ...}
+    result = 0
+    l = len(num)
+
+    for i in range(l):
+        if num[-1].isupper():
+            result += n_set[num.pop()] * (n ** i)
+        elif num[-1].isdigit():
+            result += int(num.pop()) * (n ** i)
+
+    return result
+
+
+N, B = input().split()
+
+print(notation(list(N), int(B)))
