104a2481c957dbfe779d5235e2a9469fe7160bc9
Jinho <originalll94@gmail.com>
2020-08-31 18:57:34 +0900

    math NO.11576 Base Conversion

---
+++ justzino/math/11576.py
@@ -0,0 +1,21 @@
+# a진법 -> b진법
+def notation(num, n, a, b):
+    tmp, result = 0, []
+
+    for i in range(num):
+        tmp += (n.pop() * (a ** i))
+    while tmp >= b:
+        remainder = tmp % b
+        tmp = tmp // b
+        result.append(remainder)
+    if tmp > 0:
+        result.append(tmp)
+
+    return reversed(result)
+
+
+A, B = map(int, input().split())
+m = int(input())
+nums = list(map(int, input().split()))
+
+print(*notation(m, nums, A, B))
