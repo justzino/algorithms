3cdeea8b43ee43820dc3126e7bde9654b24af19c
Jinho <originalll94@gmail.com>
2020-09-29 16:47:29 +0900

    DP 11722. 가장 긴 감소하는 부분 수열

---
+++ justzino/DP/11722.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+
+N = int(stdin.readline())
+seq = list(map(int, stdin.readline().split()))
+
+dp = [[0 for _ in range(N)] for _ in range(N)]
+longest_seq_len = 0
+
+for i in range(N):
+    longest_seq_len = max(longest_seq_len, dp[i][i])
+    for j in range(i, N):
+        if seq[i] > seq[j]:
+            dp[i][j] = dp[i][i] + 1
+            dp[j][j] = max(dp[i][j], dp[j][j])      # dp[j][j] 에 가장 큰 수 저장
+
+longest_seq_len += 1
+print(longest_seq_len)
