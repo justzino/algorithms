f0234ebe29891b14c82f77cf216e921d31b870ee
Jinho <originalll94@gmail.com>
2021-01-10 18:07:27 +0900

    Add BruteForece(M,N) No.15651

---
+++ justzino/BruteForce/15651.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+nums = [i for i in range(1, N+1)]
+visited_list = [False for _ in range(N)]
+result = list()
+
+
+def dfs(cnt):
+    if cnt == M:
+        print(*result)
+        return
+
+    for i in range(N):
+        result.append(nums[i])
+        dfs(cnt + 1)
+        result.pop()
+
+
+dfs(0)
