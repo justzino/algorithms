8ef6fa10eafab7282e0fcda3f70095964c542344
Jinho <originalll94@gmail.com>
2021-01-19 17:55:10 +0900

    Add BruteForece No.15664.N과 M (10)

---
+++ justzino/BruteForce/15664.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+visited = [False for _ in range(N)]
+results = list()
+result = list()
+
+
+def dfs(cnt):
+    if cnt == M:
+        results.append(result.copy())
+        return
+
+    for i in range(N):
+        if visited[i]:
+            continue
+        if result and result[-1] > nums[i]:
+            continue
+        result.append(nums[i])
+        visited[i] = True
+        dfs(cnt+1)
+        result.pop()
+        visited[i] = False
+
+
+dfs(0)
+remove_dup_results = list(set(map(tuple, results)))
+remove_dup_results.sort()
+for result in remove_dup_results:
+    print(*result)
