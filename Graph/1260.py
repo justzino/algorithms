a8310050ca1912ebcfae8b8b9178b4c0be59f1fb
Jinho <originalll94@gmail.com>
2021-03-22 15:46:56 +0900

    BruteForce: 1260.DFS 와 BFS

--- Graph/1260.py
+++ Graph/1260.py
@@ -2,28 +2,8 @@
 from collections import deque
 
 
-def dfs(v):
-    print(v, end=" ")
-    visited[v] = True
-    for e in adj[v]:
-        if not (visited[e]):
-            dfs(e)
-
-
-def bfs(v):
-    q = deque([v])
-    while q:
-        v = q.popleft()
-        if not (visited[v]):
-            visited[v] = True
-            print(v, end=" ")
-            for e in adj[v]:
-                if not visited[e]:
-                    q.append(e)
-
-
 n, m, v = map(int, input().split())
-adj = [[] for _ in range(n + 1)]
+adj = [[] for _ in range(n+1)]
 
 for _ in range(m):
     x, y = map(int, input().split())
@@ -33,8 +13,30 @@ for _ in range(m):
 for e in adj:
     e.sort()
 
-visited = [False] * (n + 1)
+
+def dfs(vertex):
+    print(vertex, end=' ')
+    visited[vertex] = True
+    for edge in adj[vertex]:
+        if not visited[edge]:
+            dfs(edge)
+
+
+def bfs(vertex):
+    q = deque([vertex])
+    while q:
+        vertex = q.popleft()
+        if not visited[vertex]:
+            visited[vertex] = True
+            print(vertex, end=' ')
+            for edge in adj[vertex]:
+                if not visited[edge]:
+                    q.append(edge)
+
+
+visited = [False] * (n+1)
 dfs(v)
 print()
-visited = [False] * (n + 1)
+
+visited = [False] * (n+1)
 bfs(v)
