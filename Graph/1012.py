d13d2f62ee6611c738ddbe08d0bafcacad888fba
Jinho <originalll94@gmail.com>
2021-03-24 16:54:06 +0900

    Graph/bfs: 1012.유기농 배추

--- Graph/1012.py
+++ Graph/1012.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(100000)
 
 
@@ -11,6 +12,22 @@ def dfs(x, y):
             continue
         if cabbage[nx][ny] and not visited[nx][ny]:
             dfs(nx, ny)
+
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    while q:
+        v = q.popleft()
+        if not visited[v[0]][v[1]]:
+            visited[v[0]][v[1]] = True
+            for dx, dy in directions:
+                nx, ny = v[0] + dx, v[1] + dy
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if cabbage[nx][ny] and not visited[nx][ny]:
+                    q.append((nx, ny))
 
 
 for _ in range(int(input())):
@@ -27,7 +44,8 @@ for _ in range(int(input())):
     for i in range(n):
         for j in range(m):
             if cabbage[i][j] and not visited[i][j]:
-                dfs(i, j)
+                # dfs(i, j)
+                bfs(i, j)
                 count += 1
 
     print(count)
