619735091120b512b1cb3b12c3e93b6461690943
Jinho <originalll94@gmail.com>
2021-04-13 20:53:13 +0900

    Implementation: 4-1 상하좌우 - sol

--- dbn/Implementation/4-1.py
+++ dbn/Implementation/4-1.py
@@ -1,31 +1,26 @@
 n = int(input())
-data = list(map(str, input().split()))
+plans = list(map(str, input().split()))
 
-start = [1, 1]
+x, y = 1, 1
 
-for direction in data:
-    if direction == 'U':
-        if start[0] == 1:
-            continue
-        else:
-            start[0] += -1
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
 
-    elif direction == 'D':
-        if start[0] == 5:
-            continue
-        else:
-            start[0] += 1
+# 이동 계획을 하나씩 확인
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-    elif direction == 'L':
-        if start[1] == 1:
-            continue
-        else:
-            start[1] += -1
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or nx > 5 or ny < 1 or ny > n:
+        continue
 
-    elif direction == 'R':
-        if start[1] == 5:
-            continue
-        else:
-            start[1] += 1
+    # 이동
+    x, y = nx, ny
 
-print(start[0], start[1])
+print(x, y)
