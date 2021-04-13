7dd35c29d766dbaf14b9f7d8daa7d43574d48872
Jinho <originalll94@gmail.com>
2021-04-13 17:53:25 +0900

    Greedy: 1이 될 때까지 - sol

--- dbn/Greedy/3-4.py
+++ dbn/Greedy/3-4.py
@@ -1,16 +1,19 @@
 n, k = map(int, input().split())
-
 result = 0
+
 while True:
-    if n <= 1:
+    # N 이 k 로 나누어 떨어지는 수가 될 때까지 1씩 빼는 것 한번에 계산
+    target = (n // k) * k
+    result += (n - target)
+    n = target
+
+    # n이 k보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
+    if n < k:
+        # 마지막 남은 수에 대해 1을 뺀 후 더함
+        result += (n - 1)
         break
 
-    q, r = divmod(n, k)
-    if r == 0:
-        result += 1
-        n = q
-    else:
-        result += 1
-        n = n - 1
+    result += 1
+    n //= k
 
 print(result)
