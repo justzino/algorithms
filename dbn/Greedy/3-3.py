baab1502785c70ffa8460e130aed815ed7187d04
Jinho <originalll94@gmail.com>
2021-04-13 17:27:05 +0900

    Greedy: 3-3 숫자 카드 게임 주석 추가

--- dbn/Greedy/3-3.py
+++ dbn/Greedy/3-3.py
@@ -4,6 +4,8 @@ min_nums = [0 for _ in range(n)]
 
 for i in range(n):
     data[i] = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수
     min_nums[i] = min(data[i])
 
+# 가장 작은 수들 중 가장 큰 수 츌력
 print(max(min_nums))
