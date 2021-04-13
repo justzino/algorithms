d328826f25de579a1a4d489e8c0e471c8dda1557
Jinho <originalll94@gmail.com>
2021-04-13 21:43:18 +0900

    Implementation: 4-2 시각 - sol

--- This-is-Coding-Test-Book/Implementation/4-2.py
+++ This-is-Coding-Test-Book/Implementation/4-2.py
@@ -1,13 +1,14 @@
-n = int(input())
+# 완전 탐색(Brute Forcing)
+# 탐색 해야할 데이터의 개수가 100만개 이하일 때 사용.
 
-answer_case = [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43, 53]       # 분, 초 에 나올 수 있는(60 이하의 수 중) 3 이 들어가는 수들
-n_case = len(answer_case)       # 60이하의 수 중 3이 들어가는 수의 개수
+h = int(input())
 
-if n > 3:
-    h_case = 1
-else:
-    h_case = 0
+count = 0
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있따면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
 
-result = h_case * 60 * 60 + (n - h_case + 1) * (n_case * 60 + 60 * n_case - n_case * n_case)
-
-print(result)
+print(count)
