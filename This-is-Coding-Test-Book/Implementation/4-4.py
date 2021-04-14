224ccd0325b4005730046a7bf224effbc093925a
Jinho <originalll94@gmail.com>
2021-04-14 16:38:21 +0900

    Implementation: 4-4 게임 개발 - sol

--- This-is-Coding-Test-Book/Implementation/4-4.py
+++ This-is-Coding-Test-Book/Implementation/4-4.py
@@ -1,50 +1,67 @@
 n, m = map(int, input().split())
-x, y, d = map(int, input().split())
-map_matrix = [[] for _ in range(n)]
 
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터의 X 좌표, Y 좌표, 방향을 입력받기
+x, y, direction = map(int, input().split())
+d[x][y] = 1     # 현재 좌표 방문 처리
+
+# 전체 맵 정보를 입력 받기
+array = []
 for i in range(n):
-    map_matrix[i] = list(map(int, input().split()))
+    array.append(list(map(int, input().split())))
 
-# 0: 북, 1: 동, 2: 남, 3: 서
-# 바라보는 방향: 북, 서, 남, 동
-directions = [0, 3, 2, 1]
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-# 이동 방향: 서, 남, 동, 북
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
 
-nx, ny = 0, 0
-count = 0
+# 왼쪽으로 회전
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+
+# 시뮬레이션 시작
+count = 1
 turn_time = 0
-
 while True:
-    for i in range(4):
-        turn_time += 1
-        if d != directions[i]:
-            continue
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
 
-        nx = x + dx[i]
-        ny = y + dy[i]
-        d = (i + 1) % 3
-
-        if nx < 0 or ny < 0 or nx >= n or ny >= m:
-            continue
-
-        if map_matrix[nx][ny] == 1:
-            continue
-
-        map_matrix[nx][ny] = 1
+    # 회전한 이후 정면에 가보지 않은 칸이 존재 하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
         x = nx
         y = ny
+        count += 1
         turn_time = 0
-        break
+        continue
 
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+
+    # 네 방향 모두 갈 수 없는 경우
     if turn_time == 4:
-        break
+        # 뒤로 후진
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있으면 이동
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
 
 print(count)
 
-# 입력
 # 4 4
 # 1 1 0
 # 1 1 1 1
