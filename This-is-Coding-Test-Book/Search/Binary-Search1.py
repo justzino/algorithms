230a873d83076ea83e3968b228fb8a7577ccf2b8
Jinho <originalll94@gmail.com>
2021-04-18 20:01:29 +0900

    Update Binary Search with recursion

--- This-is-Coding-Test-Book/Search/Binary-Search1.py
+++ This-is-Coding-Test-Book/Search/Binary-Search1.py
@@ -10,11 +10,11 @@ def binary_search(array, target, start, end):
 
     # 중간점의 값보다 찾는 값이 작은 경우 -> 왼쪽 확인
     elif target < array[mid]:
-        binary_search(array[:mid], target, start, mid - 1)
+        return binary_search(array[:mid], target, start, mid - 1)
 
     # 중간점의 값보다 찾는 값이 큰 경우 -> 오른쪽 확인
     else:
-        binary_search(array[mid + 1:], target, mid + 1, end)
+        return binary_search(array[mid + 1:], target, mid + 1, end)
 
 
 # n(원소의 개수)과 target(찾고자 하는 문자열)을 입력 받기
