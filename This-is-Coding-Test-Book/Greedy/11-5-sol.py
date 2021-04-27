353d8c31029dcd55450c387de114fe25ff0d2510
Jinho <originalll94@gmail.com>
2021-04-27 23:48:15 +0900

    Greedy: 11-5 볼링공 고르기 - me

--- This-is-Coding-Test-Book/Greedy/11-5-sol.py
+++ This-is-Coding-Test-Book/Greedy/11-5-sol.py
@@ -3,15 +3,12 @@ data = list(map(int, input().split()))
 
 array = [0] * 11
 
-for i in data:
-    array[i] += 1
+for x in data:
+    array[x] += 1
 
 result = 0
-
 for i in range(1, m + 1):
-    for j in range(i + 1, n):
-        if data[i] == data[j]:
-            continue
-        result += 1
+    n -= array[i]
+    result += array[i] * n
 
 print(result)
