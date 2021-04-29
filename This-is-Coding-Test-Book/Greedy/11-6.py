393da3afbb633ed3993c42b5a0301fb886d266d4
Jinho <originalll94@gmail.com>
2021-04-30 00:14:55 +0900

    Greedy: 11-6 2019 카카오 무지의 먹방 라이브 - me

--- This-is-Coding-Test-Book/Greedy/11-6.py
+++ This-is-Coding-Test-Book/Greedy/11-6.py
@@ -3,16 +3,16 @@ from collections import deque
 
 def solution(food_times, k):
     n = len(food_times)
-    q = deque(food_times)
+    q = deque()
 
     for i in range(n):
-        q.append([i, food_times[i]])
+        q.append((i, food_times[i]))
 
     while q:
         food, time = q.popleft()
 
         if k == 0:
-            return food
+            return food + 1
 
         k -= 1
         time -= 1
@@ -24,4 +24,4 @@ def solution(food_times, k):
     return -1
 
 
-solution([3, 1, 2],	5)
+print(solution([3, 1, 2], 5))
