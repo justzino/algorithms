aa66b93bb270de6ad58fbb11ab796bd8e2ccdf5e
Jinho <originalll94@gmail.com>
2021-05-03 17:18:24 +0900

    Imple-: 2020 카카오 자물쇠와 열쇠 (시간초과 테케 해결을 위한 시도) - me/실패

--- This-is-Coding-Test-Book/Implementation/12-10.py
+++ This-is-Coding-Test-Book/Implementation/12-10.py
@@ -9,18 +9,32 @@ def turn(key, n):
 
 
 # key를 (x, y) 만큼 평행 이동, N x N 만큼 자르기 / n: lock 크기
-def move(key, x, y, lock_len):
-    max_key_index = len(key) - 1
-    result = [[0] * lock_len for _ in range(lock_len)]
-    for i in range(lock_len):
-        for j in range(lock_len):
-            try:
-                if i+x < 0 or j+y < 0:
-                    continue
-                result[i+x][j+y] = key[i][j]
+# def move(key, x, y, lock_len):
+#     result = [[0] * lock_len for _ in range(lock_len)]
+#     for i in range(lock_len):
+#         for j in range(lock_len):
+#             try:
+#                 if i+x < 0 or j+y < 0:
+#                     continue
+#                 result[i+x][j+y] = key[i][j]
+#
+#             except:
+#                 continue
+#
+#     return result
 
-            except:
+# 값이 1인 key만 (x, y) 만큼 평행 이동, N x N 만큼 자르기 / n: lock 크기
+def move(keys, key, x, y, lock_len):
+    result = [[0] * lock_len for _ in range(lock_len)]
+    for k in keys:
+        i, j = k[0], k[1]
+        try:
+            if i+x < 0 or j+y < 0:
                 continue
+            result[i+x][j+y] = key[i][j]
+
+        except:
+            continue
 
     return result
 
@@ -42,23 +56,29 @@ def solution(key, lock):
     keys, locks, gaps = [], [], []
 
     for c in range(4):
-        # key 에서 값이 1인 부분을 lock 에서 값이 0인 부분에 맞춰서 move 하고 모두 비교
-        # key == 1인 부분 뽑기
-        for i in range(key_len):
-            for j in range(key_len):
-                if key[i][j] == 1:
-                    keys.append((i, j))
-
+        # key 에서 값이 1인 부분을 lock 에서 값이 0인 부분에 맞춰서 move 하면서 모두 비교
         # lock == 0인 부분 뽑기
         for i in range(lock_len):
             for j in range(lock_len):
                 if lock[i][j] == 0:
                     locks.append((i, j))
 
+        # 예외처리: lock 이 전부 1인 경우
         if not locks:
             return True
 
+        # key == 1인 부분 뽑기
+        for i in range(key_len):
+            for j in range(key_len):
+                if key[i][j] == 1:
+                    keys.append((i, j))
+
+        # 예외처리: lock 에 0 이 있는데 key 가 전부 0 인 경우
         if not keys:
+            return False
+        
+        # 예외처리: lock 0 의 개수 보다 key 1의 개수가 적은 경우 
+        if len(keys) < len(locks):
             return False
 
         # key == 1 인 부분을 lock == 0 인 부분으로 move 할 (x, y) 구하기
@@ -68,7 +88,8 @@ def solution(key, lock):
 
         # 하나씩 move 시키며 비교
         for gap in gaps:
-            m_key = move(key, gap[0], gap[1], lock_len)
+            # m_key = move(key, gap[0], gap[1], lock_len)
+            m_key = move(keys, key, gap[0], gap[1], lock_len)
             result = unlock(m_key, lock, lock_len)
 
             if result:
@@ -78,7 +99,7 @@ def solution(key, lock):
 
         if c == 3:
             return False
+
+        # key를 회전시켜서 다시 탐색
         key = turn(key, key_len)
 
-
-# print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
