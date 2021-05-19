4b69fba704afa720654b3ec8870a2ed3d3f9cae0
Jinho <originalll94@gmail.com>
2021-05-19 15:32:09 +0900

    FIX(Imple-): 12-14 2020 카카오 외벽 점검 - sol참고

--- This-is-Coding-Test-Book/Implementation/12-14-sol.py
+++ This-is-Coding-Test-Book/Implementation/12-14-sol.py
@@ -7,13 +7,13 @@ def solution(n, weak, dist):
 
     for i in range(length):
         weak.append(weak[i] + n)
-    answer = len(dist) + 1 # 투입할 친구 수의 최솟값을 찾아야 하므로 len(dist) + 1로 초기화
+    answer = len(dist) + 1      # 투입할 친구 수의 최솟값을 찾아야 하므로 len(dist) + 1로 초기화
 
     # 0부터 length - 1까지의 위치를 각각 시작점으로 설정
     for start in range(length):
         # 친구를 나열하는 모든 경우 각각에 대하여 확인
         for friends in list(permutations(dist, len(dist))):
-            count = 1 # 투입할 친구의 수
+            count = 1   # 투입할 친구의 수
             # 해당 친구가 점검할 수 있는 마지막 위치
             position = weak[start] + friends[count - 1]
 
@@ -21,11 +21,12 @@ def solution(n, weak, dist):
             for index in range(start, start + length):
                 # 점검할 수 있는 위치를 벗어나는 경우
                 if position < weak[index]:
-                    count += 1 # 새로운 친구를 투입
-                    if count > len(dist): # 더 투입이 불가능하다면 종료
+                    count += 1      # 새로운 친구를 투입
+                    if count > len(dist):   # 더 투입이 불가능하다면 종료
                         break
                     position = weak[index] + friends[count - 1]
-            answer = min(answer, count) # 최솟값 계산
+            answer = min(answer, count)     # 최솟값 계산
+
     if answer > len(dist):
         return -1
 
