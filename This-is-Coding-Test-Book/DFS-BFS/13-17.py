4d878400b7ef9f2276a06e225a7821b2cf786045
Jinho <originalll94@gmail.com>
2021-05-22 13:05:46 +0900

    BFS: 13-17 18405.경쟁적 전염 - me

---
+++ This-is-Coding-Test-Book/DFS-BFS/13-17.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+
+# 우, 하, 좌, 상
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+def widen(virus, time):
+    num_virus = len(virus)
+    for _ in range(time):
+        for _ in range(num_virus):
+            if not virus:
+                break
+            num, x, y = virus.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                    continue
+                if matrix[nx][ny] > 0:
+                    continue
+                matrix[nx][ny] = num
+                virus.append([num, nx, ny])
+
+
+n, k = map(int, input().split())
+
+matrix = []
+q = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    matrix.append(tmp)
+    for j in range(n):
+        if tmp[j] > 0:
+            q.append([tmp[j], i, j])
+
+q.sort()
+q = deque(q)
+
+time, x, y = map(int, input().split())
+
+widen(q, time)
+
+print(matrix[x-1][y-1])
