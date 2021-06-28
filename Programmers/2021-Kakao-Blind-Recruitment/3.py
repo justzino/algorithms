9c5270bf1ccc166520787ed04331fde0fb2e540f
Jinho <originalll94@gmail.com>
2021-06-29 02:46:35 +0900

    2021 카카오 공채 3.순위 검색 - 효율성통과

--- Programmers/2021-Kakao-Blind-Recruitment/3.py
+++ Programmers/2021-Kakao-Blind-Recruitment/3.py
@@ -1,49 +1,47 @@
-def solution(info, query):
-    lang = {'cpp': 0, 'java': 1, 'python': 2, '-': -1}
-    app = {'backend': 0, 'frontend': 1, '-': -1}
-    career = {'junior': 0, 'senior': 1, '-': -1}
-    food = {'chicken': 0, 'pizza': 1, '-': -1}
+from bisect import bisect_left
 
-    table = []
+
+def solution(info, query):
+    lang = {'cpp': 1, 'java': 2, 'python': 3, '-': 0}
+    app = {'backend': 1, 'frontend': 2, '-': 0}
+    career = {'junior': 1, 'senior': 2, '-': 0}
+    food = {'chicken': 1, 'pizza': 2, '-': 0}
+
+    table = {}
+
     for row in info:
         tmp = list(row.split())
         tmp[0] = lang[tmp[0]]
         tmp[1] = app[tmp[1]]
         tmp[2] = career[tmp[2]]
         tmp[3] = food[tmp[3]]
-        tmp[4] = int(tmp[4])
-        table.append(tmp)
+        score = int(tmp[4])
 
-    query_list = []
-    for q in query:
-        tmp = list(q.split(' and '))
-        tmp[0] = lang[tmp[0]]
-        tmp[1] = app[tmp[1]]
-        tmp[2] = career[tmp[2]]
+        for i in range(16):
+            case = bin(i)[2:].zfill(4)
+            string = str(tmp[0] * int(case[0])) + str(tmp[1] * int(case[1])) + str(tmp[2] * int(case[2])) + str(
+                tmp[3] * int(case[3]))
+            try:
+                table[string].append(score)
+            except:
+                table[string] = [score]
 
-        f, s = tmp[3].split()
-        tmp[3] = food[f]
-        tmp.append(int(s))
-
-        query_list.append(tmp)
-
-    print(table)
-    print(query_list)
+    new_table = {}
+    for key, value in table.items():
+        value.sort()
+        new_table[key] = value
 
     result = []
-    for qur in query_list:
-        search = 0
-        for x in table:
-            if not (qur[0] == -1 or x[0] == qur[0]):
-                continue
-            if not (qur[1] == -1 or x[1] == qur[1]):
-                continue
-            if not (qur[2] == -1 or x[2] == qur[2]):
-                continue
-            if not (qur[3] == -1 or x[3] == qur[3]):
-                continue
-            if x[4] >= qur[4]:
-                search += 1
-        result.append(search)
+    for q in query:
+        tmp = list(q.split(' and '))
+        f, score = tmp[3].split()
+        string = str(lang[tmp[0]]) + str(app[tmp[1]]) + str(career[tmp[2]]) + str(food[f])
+        score = int(score)
+
+        try:
+            index = bisect_left(new_table[string], score)
+            result.append(len(new_table[string]) - index)
+        except:
+            result.append(0)
 
     return result
