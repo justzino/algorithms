911eec1f018cf022170fadb12a3ea656259deade
Jinho <originalll94@gmail.com>
2021-06-29 02:46:18 +0900

    2021 카카오 공채 3.순위 검색 - 효율성통과x/refactor

--- Programmers/2021-Kakao-Blind-Recruitment/3.py
+++ Programmers/2021-Kakao-Blind-Recruitment/3.py
@@ -1,43 +1,49 @@
 def solution(info, query):
-    # lang = {'cpp': 0, 'java': 1, 'python': 2}
-    # app = {'backend': 0, 'frontend': 1}
-    # career = {'junior': 0, 'senior': 1}
-    # food = {'chicken': 0, 'pizza': 1}
+    lang = {'cpp': 0, 'java': 1, 'python': 2, '-': -1}
+    app = {'backend': 0, 'frontend': 1, '-': -1}
+    career = {'junior': 0, 'senior': 1, '-': -1}
+    food = {'chicken': 0, 'pizza': 1, '-': -1}
 
     table = []
     for row in info:
         tmp = list(row.split())
-        tmp[-1] = int(tmp[-1])
+        tmp[0] = lang[tmp[0]]
+        tmp[1] = app[tmp[1]]
+        tmp[2] = career[tmp[2]]
+        tmp[3] = food[tmp[3]]
+        tmp[4] = int(tmp[4])
         table.append(tmp)
 
     query_list = []
     for q in query:
-        row = list(q.split(' and '))
-        f, s = row[-1].split()
-        row[-1] = f
-        row.append(int(s))
-        query_list.append(row)
+        tmp = list(q.split(' and '))
+        tmp[0] = lang[tmp[0]]
+        tmp[1] = app[tmp[1]]
+        tmp[2] = career[tmp[2]]
+
+        f, s = tmp[3].split()
+        tmp[3] = food[f]
+        tmp.append(int(s))
+
+        query_list.append(tmp)
+
+    print(table)
+    print(query_list)
 
     result = []
-    for case in query_list:
-        if case[0] == '-':
-            search = table
-        else:
-            search = [x for x in table if x[0] == case[0]]
-        if case[1] == '-':
-            pass
-        else:
-            search = [x for x in search if x[1] == case[1]]
-        if case[2] == '-':
-            pass
-        else:
-            search = [x for x in search if x[2] == case[2]]
-        if case[3] == '-':
-            pass
-        else:
-            search = [x for x in search if x[3] == case[3]]
-        search = [x for x in search if x[4] >= case[4]]
-
-        result.append(len(search))
+    for qur in query_list:
+        search = 0
+        for x in table:
+            if not (qur[0] == -1 or x[0] == qur[0]):
+                continue
+            if not (qur[1] == -1 or x[1] == qur[1]):
+                continue
+            if not (qur[2] == -1 or x[2] == qur[2]):
+                continue
+            if not (qur[3] == -1 or x[3] == qur[3]):
+                continue
+            if x[4] >= qur[4]:
+                search += 1
+        result.append(search)
 
     return result
