f0bbdce33b1a4e30ebe48c58540991a8111a5521
Jinho <originalll94@gmail.com>
2021-08-03 00:15:25 +0900

    Stack-Queue: 2.프린터

---
+++ Programmers/Stack-Queue/2.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+def solution(priorities, location):
+    nums_sorted = sorted(priorities)
+
+    q = deque([(priorities[i], i) for i in range(len(priorities))])
+
+    answer = 1
+    while True:
+        if q[0][0] < nums_sorted[-1]:
+            q.append(q.popleft())
+            continue
+        nums_sorted.pop()
+        now = q.popleft()
+
+        if now[1] == location:
+            break
+        answer += 1
+
+    return answer
