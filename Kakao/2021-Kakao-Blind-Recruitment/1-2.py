5d75fce2f04e77983625b657066187e32dcd4d1d
Jinho <originalll94@gmail.com>
2021-08-13 16:53:50 +0900

    CHORE: Move directory of KAKAO

--- Kakao/2021-Kakao-Blind-Recruitment/1-2.py
+++ Kakao/2021-Kakao-Blind-Recruitment/1-2.py
@@ -4,7 +4,7 @@ import re
 def solution(new_id):
     string = new_id.lower()    # 1단계
     string = re.sub('[^a-z0-9\-_.]', '', string)    # 2단계
-    string = re.sub('[.]+', '.', string)            # 3단계
+    string = re.sub('[.]+', '', string)            # 3단계
     string = re.sub('^[.]|[.]$', '', string)        # 4단계
     string = 'a' if not string else string[:15]    # 5, 6단계
     string = re.sub('[.]$', '', string)
