97ad2e1716914274d70e04e89e88af343fea0a9f
Jinho <originalll94@gmail.com>
2021-09-10 17:15:10 +0900

    [2018 카카오공채] 1.비밀지도

--- Kakao/2018-Kakao-Blind-recruitment/1.py
+++ Kakao/2018-Kakao-Blind-recruitment/1.py
@@ -1,28 +1,11 @@
-def change(n, arr):
-    result = []
-    for i in arr:
-        string = str(bin(i))[2:].zfill(n)
-        result.append(list(map(int, string)))
-    return result
-
-
-def sum_arr(n, arr1, arr2):
-    array = []
-    for i in range(n):
-        row = ''
-        for j in range(n):
-            value = arr1[i][j] or arr2[i][j]
-            if value == 1:
-                row += '#'
-            else:
-                row += ' '
-        array.append(row)
-    return array
-
-
 def solution(n, arr1, arr2):
-    array1 = change(n, arr1)
-    array2 = change(n, arr2)
-    answer = sum_arr(n, array1, array2)
+    result = []
+    for i in range(n):
+        tmp = arr1[i] | arr2[i]
+        tmp = bin(tmp)[2:]
+        tmp = tmp.zfill(n)
+        tmp = tmp.replace('1', '#')
+        tmp = tmp.replace('0', ' ')
+        result.append(tmp)
 
-    return answer
+    return result
