a07f4656948633a61ea064896a83c470205319e2
Jinho <originalll94@gmail.com>
2021-10-27 18:55:51 +0900

    dp: 11066.파일 합치기 - 시간 초과

---
+++ Baekjoon/11066-dp.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+INF = 10001
+
+for _ in range(T):
+    N = int(input())
+    pages = [0] + list(map(int, input().split()))
+    dp = [[0 for j in range(N+1)] for i in range(N+1)]
+    sums = [0 for _ in range(N+1)]
+    for i in range(1, N+1):
+        sums[i] = sums[i-1] + pages[i]
+
+    for j in range(2, N+1):
+        for i in range(j-1, 0, -1):
+            values = [dp[i][k] + dp[k+1][j] + sums[j]-sums[i-1] for k in range(i, j)]
+            dp[i][j] = min(values)
+    print(dp[1][N])
